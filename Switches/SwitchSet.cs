1e83221a6d51a0696e912519141b8b245a37f37c
lzcd <lzcd@hotmail.com>
2011-07-27 15:16:24 +1000

    Added solution

---
+++ Switches/SwitchSet.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Dynamic;
+
+namespace Switches
+{
+    public class SwitchSet : DynamicObject
+    {
+        private Dictionary<string, string> settingByName;
+        private HashSet<string> switches;
+
+        public SwitchSet(string[] args)
+        {
+            settingByName = new Dictionary<string, string>();
+            switches = new HashSet<string>();
+
+            var lastName = default(string);
+
+            foreach (var token in args)
+            {
+                if (token.StartsWith("-"))
+                {
+                    lastName = token.Substring(1);
+                    switches.Add(lastName);
+                }
+                else
+                {
+                    switches.Remove(lastName);
+                    settingByName[lastName] = token;
+                }
+            }
+        }
+
+        private const string settingExistsSuffix = "settingexists";
+        private const string settingSuffix = "setting";
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            var bindingName = binder.Name.ToLower();
+
+            
+            if (bindingName.EndsWith(settingExistsSuffix))
+            {
+                var settingName = binder.Name.Substring(0, binder.Name.Length - settingExistsSuffix.Length);
+                result = switches.Contains(settingName) || settingByName.ContainsKey(settingName);
+                return true;
+            }
+
+            if (bindingName.EndsWith(settingSuffix))
+            {
+                var settingName = binder.Name.Substring(0, binder.Name.Length - settingSuffix.Length);
+
+                if (switches.Contains(settingName))
+                {
+                    result = true;
+                    return true;
+                }
+
+                var setting = default(string);
+                if (settingByName.TryGetValue(settingName, out setting))
+                {
+                    result = setting;
+                    return true;
+                }
+            }
+
+            result = null;
+            return false;
+        }
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Switches;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var args = new string[] { 
+                @"-d", @"My Database",
+                @"-q",
+                @"-a",
+                @"-L", @"C:\log.txt",
+                @"-s", "." };
+
+
+            dynamic switchSet = new SwitchSet(args);
+            Assert.AreEqual(true, switchSet.dSettingExists);
+            Assert.AreEqual(@"My Database", switchSet.dSetting);
+            Assert.AreEqual(true, switchSet.qSettingExists);
+            Assert.AreEqual(false, switchSet.xSettingExists);
+        }
+    }
+}
