805c602e15c558b87259e3d2449ff6a02e422183
lzcd <lzcd@hotmail.com>
2011-07-27 16:29:33 +1000

    Added fluent expectation style processing

---
+++ Switches/SwitchPath.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Switches
+{
+    public class SwitchPath
+    {
+        private string tokenName;
+        private Dictionary<string, Action<string>> actionByTokenName = new Dictionary<string, Action<string>>();
+
+        public SwitchPath On(string token)
+        {
+            tokenName = token;
+            return this;
+        }
+
+        public SwitchPath Do(Action action)
+        {
+            actionByTokenName[tokenName] = (s) => { action(); };
+            return this;
+        }
+
+        public SwitchPath Do(Action<string> action)
+        {
+            actionByTokenName[tokenName] = (s) => { action(s); };
+            return this;
+        }
+
+        public SwitchPath Ignore()
+        {
+            actionByTokenName[tokenName] = (s) => { };
+            return this;
+        }
+
+        private Action<string> onRemainder;
+
+        public SwitchPath OnRemainderDo(Action<string> action)
+        {
+            onRemainder = action;
+            return this;
+        }
+
+        public void Parse(string[] args)
+        {
+            var lastName = default(string);
+            var remaining = new List<string>();
+
+            foreach (var token in args)
+            {
+                Action<string> action = null;
+                if (token.StartsWith("-"))
+                {
+                    if (lastName != null)
+                    {
+                        if (actionByTokenName.TryGetValue(lastName, out action))
+                        {
+                            action("");
+                            action = null;
+                        }
+                        else 
+                        {
+                            remaining.Add(token);
+                        }
+                    }
+
+                    lastName = token.Substring(1);
+                    continue;
+                }
+
+
+                
+                if (lastName != null)
+                {
+
+                    if (!actionByTokenName.TryGetValue(lastName, out action))
+                    {
+                        remaining.Add(lastName);
+                        lastName = null;
+                        continue;
+                    }
+                }
+                else
+                {
+                    if (!actionByTokenName.TryGetValue(token, out action))
+                    {
+                        remaining.Add(token);
+                        continue;
+                    }
+                }
+                action(token);
+                lastName = null;
+
+            }
+
+            foreach (var remainder in remaining)
+            {
+                onRemainder(remainder);
+            }
+        }
+
+
+    }
+}
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -11,7 +11,35 @@ namespace Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SwitchPathWorkBestest()
+        {
+            var message ="";
+            var db = "";
+            var quack = false;
+
+            var switchPath = new SwitchPath()
+                    .On("d").Do(name => { db = name; })
+                    .On("q").Do(() => { quack = true; })
+                    .On("L").Ignore()
+                    .On("s").Ignore()
+                    .OnRemainderDo(name => { message = "Why you " + name + "?"; });
+
+            var args = new string[] { 
+                @"-d", @"My Database",
+                @"-q",
+                @"-a",
+                @"-L", @"C:\log.txt",
+                @"-s", "." };
+
+            switchPath.Parse(args);
+
+            Assert.AreEqual(@"My Database", db);
+            Assert.IsTrue(quack);
+            Assert.AreEqual(@"Why you -L?", message);
+        }
+
+        [TestMethod]
+        public void SwitchSetsDoesWorkGoodest()
         {
             var args = new string[] { 
                 @"-d", @"My Database",
